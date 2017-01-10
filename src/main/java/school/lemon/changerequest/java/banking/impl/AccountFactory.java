b877e7ed25bf9b312e422e93d894caedda88928e
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-10 19:41:28 +0200

    AccountFactory (not all)

--- src/main/java/school/lemon/changerequest/java/banking/impl/AccountFactory.java
+++ src/main/java/school/lemon/changerequest/java/banking/impl/AccountFactory.java
@@ -1,6 +1,7 @@
 package school.lemon.changerequest.java.banking.impl;
 
 import school.lemon.changerequest.java.banking.BankAccount;
+import school.lemon.changerequest.java.banking.BankAccountImpl;
 import school.lemon.changerequest.java.banking.TransactionalBankAccount;
 
 public final class AccountFactory {
@@ -12,9 +13,10 @@ public final class AccountFactory {
      * Creates bank account
      * @return bank account
      */
-    public static BankAccount createBankAccount() {
+    public static BankAccount createBankAccount(int index, double rate, double balance) {
+        BankAccountImpl bankAccount = new BankAccountImpl(index,rate,balance);
         //TODO: implement me
-        return null;
+        return bankAccount;
     }
 
     /**
