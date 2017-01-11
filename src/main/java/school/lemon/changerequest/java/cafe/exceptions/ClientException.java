e590710f39cf943b22859ee7282bab1f5bc152da
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-11 16:33:08 +0200

    ClientException

---
+++ src/main/java/school/lemon/changerequest/java/cafe/exceptions/ClientException.java
@@ -0,0 +1,25 @@
+package school.lemon.changerequest.java.cafe.exceptions;
+
+/**
+ * Created by Diana on 11.01.2017.
+ */
+public class ClientException extends Exception {
+    public ClientException() {
+    }
+
+    public ClientException(String message) {
+        super(message);
+    }
+
+    public ClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ClientException(Throwable cause) {
+        super(cause);
+    }
+
+    public ClientException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
