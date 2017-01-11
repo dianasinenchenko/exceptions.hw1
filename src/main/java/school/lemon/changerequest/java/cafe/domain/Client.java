b87b747f4146faf1d2d2f49712d7cb6d0330c20f
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-11 18:01:47 +0200

    Client

--- src/main/java/school/lemon/changerequest/java/cafe/domain/Client.java
+++ src/main/java/school/lemon/changerequest/java/cafe/domain/Client.java
@@ -17,14 +17,21 @@ public class Client {
                     String.format("Tastes like a bla bla %d", drink.getDrinkType()));
 
         } else if (drink.getTemperature() < LOWEST_TEMPERATURE) {
-            System.out.println("Looks like it's too cold...");
+            throw new ClientException(
+                    String.format("Looks like it's too cold... "));
+
         } else if (drink.getTemperature() > HIGHEST_TEMPERATURE) {
-            System.out.println("Looks like it's too hot...");
+            throw new ClientException(
+                    String.format("Looks like it's too hot... "));
+
         } else if (random.nextInt(100) < 10) {
-            System.out.println("I don't like your Cafe!");
+            throw new ClientException(
+                    String.format("I don't like your Cafe!"));
+
         } else {
-            System.out.println("Delicious, thank you.");
-        }
+            throw new ClientException(
+                    String.format("Delicious, thank you."));
+                  }
     }
 
 }
