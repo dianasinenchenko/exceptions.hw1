d4d7d0405fcce08f391de091d2cef562d66dd9b1
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-11 17:25:39 +0200

    Cafe

--- src/main/java/school/lemon/changerequest/java/cafe/domain/Cafe.java
+++ src/main/java/school/lemon/changerequest/java/cafe/domain/Cafe.java
@@ -1,13 +1,19 @@
 package school.lemon.changerequest.java.cafe.domain;
 
+import school.lemon.changerequest.java.cafe.exceptions.ClientException;
+
 import java.util.Random;
 
 public class Cafe {
 
     private final Random random = new Random();
 
-    public void serve(Client client) {
-        client.drinkCoffee(generateCup());
+    public void serve(Client client) throws ClientException {
+        try {
+            client.drinkCoffee(generateCup());
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
     }
 
     private Drink generateCup() {
