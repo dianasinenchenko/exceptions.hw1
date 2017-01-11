53babc4eb26a07eceb52f0cf16deaeaf99dd12e7
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-11 17:26:13 +0200

    App

--- src/main/java/school/lemon/changerequest/java/cafe/App.java
+++ src/main/java/school/lemon/changerequest/java/cafe/App.java
@@ -2,16 +2,20 @@ package school.lemon.changerequest.java.cafe;
 
 import school.lemon.changerequest.java.cafe.domain.Cafe;
 import school.lemon.changerequest.java.cafe.domain.Client;
+import school.lemon.changerequest.java.cafe.exceptions.ClientException;
 
 public class App {
+
+
+
 
     public static void main(String[] args) {
         Cafe cafe = new Cafe();
         for (int i = 0; i < 1000; ++i) {
             try {
                 cafe.serve(new Client());
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+            } catch (ClientException ex) {
+                System.out.println("run ex "+ ex.getMessage());
             }
         }
     }
