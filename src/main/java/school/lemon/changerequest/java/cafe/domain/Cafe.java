d7ad5443ed05e6d6e6dd4d3e3d6b918584a2fbc6
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-12 15:40:50 +0200

    Cafe

--- src/main/java/school/lemon/changerequest/java/cafe/domain/Cafe.java
+++ src/main/java/school/lemon/changerequest/java/cafe/domain/Cafe.java
@@ -7,13 +7,14 @@ public class Cafe {
     private final Random random = new Random();
 
     public void serve(Client client) throws Client.ClientException, Client.ClientExceptionNotACoffee {
-        try{
-        client.drinkCoffee(generateCup());
-
-
-        }
-        catch (Client.ClientExceptionNotACoffee e){
-            throw new Client.ClientExceptionNotACoffee(String.format(" %s Sorry but we do not have this drink make another selection %s", e.getMessage(), generateCup().getDrinkType()));
+        try {
+            client.drinkCoffee(generateCup());
+        } catch (Client.ClientExceptionNotACoffee exceptionNotACoffee) {
+            throw new Client.ClientExceptionNotACoffee(String.format(" %s Sorry but we do not have this drink make another selection %s", exceptionNotACoffee.getMessage(), generateCup().getDrinkType()));
+        } catch (Client.ClientExceptionTemperatureIsHot exceptionTemperatureIsHot) {
+            throw new Client.ClientExceptionTemperatureIsHot(String.format("%s Sorry we fix this now %s ", exceptionTemperatureIsHot.getMessage(), exceptionTemperatureIsHot.getCause()));
+        } catch (Client.ClientExceptionTemperatureIsCold exceptionTemperatureIsCold) {
+            throw new Client.ClientExceptionTemperatureIsCold(String.format("%s Sorry we fix this now %s", exceptionTemperatureIsCold.getMessage(), exceptionTemperatureIsCold.getCause()));
         }
 
     }
